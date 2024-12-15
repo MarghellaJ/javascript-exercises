e9066443ca7be033ed77bc4ef61e7037cf3291ee
Jim <thatdeveloperjim@gmail.com>
2024-12-15 12:14:03 -0500

    removeFromArray
    lesson 04 removeFromArray competed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+   // create new array
+   const newArray = [];
+   
+   // loop through array
+   // push items in array into newArray UNLESS they are the argument
+   array.forEach(item => {
+      if (!args.includes(item)){
+         newArray.push(item);
+      }
+   });
+
+   //return newArray with arguments removed
+   return newArray;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
