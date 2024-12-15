34a0e652017ae8d574a5683be2a1e2482e780d3d
Jim <thatdeveloperjim@gmail.com>
2024-12-15 14:32:30 -0500

    lesson 05 sumALL all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+   if (num1 < 0 || num2 < 0) return "ERROR"; // check not negative numbers
+   if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR"; // check if integer
+   if (num1 > num2) [num1, num2] = [num2, num1]; //if 1st number larger than 2nd
+
+
+   //declare a variable
+   //loop through range
+   //add to sum
+   let sum = 0;
+   for (let i = num1; i <= num2; i++)
+      
+      sum += i
+
+   return sum;
 };
 
 // Do not edit below this line
